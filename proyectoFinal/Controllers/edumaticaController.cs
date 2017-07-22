bc0fb23b6b4a97b7d3d8ba957cc305159207aba5
ytrejo <ytrejo@QRO-YTREJO>
2017-07-22 12:07:22 -0500

    se sube controlador empleados

--- proyectoFinal/Controllers/edumaticaController.cs
+++ proyectoFinal/Controllers/edumaticaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,8 +17,13 @@ namespace proyectoFinal.Controllers
 
         public JsonResult getInformation()
         {
-            
-            return Json(, JsonRequestBehavior.AllowGet);
+            /* ws.WebService1 webService = new ws.WebService1();
+             return Json(webService.getEmpleados(), JsonRequestBehavior.AllowGet);*/
+
+            var empleados = new List<object>();
+            empleados.Add(new { nombre = "Cinthya", apellido = "Trejo", edad = "24", domicilio = "1984", ciudad="Quérétaro", estado = "Querétaro", id_departamento = 3});
+            empleados.Add(new { nombre = "Juan", apellido = "López",edad = "23", domicilio = "1939", ciudad = "Quérétaro", estado = "Querétaro", id_departamento = 1 });
+            return Json(empleados, JsonRequestBehavior.AllowGet);
         }
     }
 }
