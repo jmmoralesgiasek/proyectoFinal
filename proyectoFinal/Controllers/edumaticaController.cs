a08285db71bfed5d5fdcc0bfb245cca31a13409d
ytrejo <ytrejo@QRO-YTREJO>
2017-07-22 10:53:30 -0500

    Add project files.

---
+++ proyectoFinal/Controllers/edumaticaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyectoFinal.Controllers
+{
+    public class edumaticaController : Controller
+    {
+        // GET: edumatica
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getInformation()
+        {
+            
+            return Json(, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ proyectoFinal/Scripts/funcionesComunes.js
@@ -0,0 +1,15 @@
+﻿function getInformation() {
+
+    $.ajax({
+        url: "/edumatica/getInformation",
+        cache: false,
+        type: "POST",
+        success: function (response) {
+            $("#respuesta").html(response);
+        },
+        error: function (xhr) {
+            alert("Ha ocurrido un error " + xhr);
+        }
+    });
+
+}
